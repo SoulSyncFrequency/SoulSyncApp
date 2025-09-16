322a776dd210241153c4125b65bb7cfa9cab2cd9
SoulSyncFrequency <soulsyncfrequency@gmail.com>
2025-09-17 01:45:04 +0200

    Delete drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'drizzle-kit'
-
-export default {
-  schema: './src/../drizzle/schema.ts',
-  out: './migrations',
-  dialect: 'postgresql',
-  verbose: true,
-  strict: true
-} satisfies Config
