871166a3db7c08f5ce06b888aa2d8fdea52848a9
SoulSyncFrequency <soulsyncfrequency@gmail.com>
2025-09-17 01:34:42 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli'
-const config: CapacitorConfig = { appId:'com.soulsync.app', appName:'SoulSync', webDir:'dist', server:{ androidScheme:'https' } }
-export default config
