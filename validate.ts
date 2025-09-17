b42d91470b3cd713f00dd8dbbf85f9c4d0c10264
SoulSyncFrequency <soulsyncfrequency@gmail.com>
2025-09-17 02:01:46 +0200

    Delete validate.ts

--- validate.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const therapyInputSchema = z.object({
-  disease: z.string().min(2).max(100),
-  symptoms: z.array(z.string()).optional(),
-  language: z.enum(['en','hr']).default('en')
-})
-
-export type TherapyInputValidated = z.infer<typeof therapyInputSchema>
