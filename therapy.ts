2704607cf6d884be5ebccc737b9831ceedad589a
SoulSyncFrequency <soulsyncfrequency@gmail.com>
2025-09-17 02:00:13 +0200

    Delete therapy.ts

--- therapy.ts
+++
@@ -1,6 +0,0 @@
-import { buildUltraPlan, UltraInput, UltraPlan } from './ultra51c.js'
-export type TherapyInput = UltraInput
-export type TherapyPlan = UltraPlan
-export function generateTherapy(input: TherapyInput): TherapyPlan {
-  return buildUltraPlan(input)
-}
