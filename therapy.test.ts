135275c152e6bd84941c43932ebe9a139699072b
SoulSyncFrequency <soulsyncfrequency@gmail.com>
2025-09-17 02:00:06 +0200

    Delete therapy.test.ts

--- therapy.test.ts
+++
@@ -1,6 +0,0 @@
-import { buildUltraPlan } from '../engine/ultra51c'
-test('plan has smiles and 5 days', () => {
-  const plan = buildUltraPlan({ disease: 'Depression' })
-  expect(plan.smiles).toBeTruthy()
-  expect(plan.plan5day.length).toBe(5)
-})
